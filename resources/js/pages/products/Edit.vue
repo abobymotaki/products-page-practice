9de867340b17a3e7e9234773fb0b4889b5f1631b
Taki Ahmed <takiakiba3@gmail.com>
2025-08-30 01:46:14 +0400

    potential fix

--- resources/js/pages/products/Edit.vue
+++ resources/js/pages/products/Edit.vue
@@ -4,6 +4,7 @@ import Input from '@/components/ui/input/Input.vue';
 import Label from '@/components/ui/label/Label.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
+import { route } from 'ziggy-js';
 
 interface Product {
     id: number;
