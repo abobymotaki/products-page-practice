5ee3af7c26ed0399183bbc33b71a23e58029f9fd
Taki Ahmed <takiakiba3@gmail.com>
2025-08-30 01:55:15 +0400

    watafuk 1

--- resources/js/pages/settings/Password.vue
+++ resources/js/pages/settings/Password.vue
@@ -11,6 +11,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { type BreadcrumbItem } from '@/types';
+import { route } from 'ziggy-js';
 
 const breadcrumbItems: BreadcrumbItem[] = [
     {
